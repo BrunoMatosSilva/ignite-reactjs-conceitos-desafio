f4e840a14a3ea8fc3ea5e8a0614f8803ad1e1459
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-03-14 19:49:18 -0300

    action extras

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { VscClearAll } from "react-icons/vsc";
+import { MdLayersClear } from "react-icons/md";
+import { FaCheckDouble } from "react-icons/fa";
 
 interface Task {
   id: number;
@@ -14,32 +17,73 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  const newTask = {
+    id: Math.random(),
+    title: newTaskTitle,
+    isComplete: false,
+  };
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    setTasks((oldState) => [...oldState, newTask]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newtask = tasks.map((task) =>
+      task.id === id
+        ? {
+          ...task,
+          isComplete: !task.isComplete,
+        }
+        : task
+    );
+
+    setTasks(newtask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksFiltered = tasks.filter((task) => task.id !== id);
+    setTasks(tasksFiltered);
+  }
+
+  function handleCompletedAll() {
+    const newtask = tasks.map((task) =>
+      task.isComplete !== true
+        ? {
+          ...task,
+          isComplete: !task.isComplete,
+        }
+        : task
+    );
+
+    setTasks(newtask);
+  }
+
+  function handleResetAllComplete() {
+    const filteredNotCompleted = tasks.filter(task => task.isComplete !== true)
+    setTasks(filteredNotCompleted)
+  }
+
+  function handleResetAll() {
+    setTasks([])
   }
 
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>My Tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Add Task"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
@@ -50,7 +94,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -62,12 +106,34 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
+        <div className="options-task">
+          <div>
+            <button type="button" onClick={() => handleCompletedAll()}>
+              <FaCheckDouble size={16} />
+            </button>
+            <span>Completed All</span>
+          </div>
+
+          <div>
+            <button type="button" onClick={() => handleResetAllComplete()}>
+              <MdLayersClear size={16} />
+            </button>
+            <span>Clear done</span>
+          </div>
+
+          <div>
+            <button type="button" onClick={() => handleResetAll()}>
+              <VscClearAll size={16} />
+            </button>
+            <span>Clear All</span>
+          </div>
+        </div>
       </main>
     </section>
   )
